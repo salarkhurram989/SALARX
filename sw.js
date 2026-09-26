bd35317c7adc7ffa0f32e4c8ea58b0d53ff902d7
Salar <salarkhurram989@gmail.com>
2026-09-26 15:20:08 +0500

    Disable and unregister legacy service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,2 @@
-const CACHE='salarx-v1';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil(self.registration.unregister().then(()=>self.clients.matchAll()).then(clients=>Promise.all(clients.map(client=>client.navigate(client.url))))));
